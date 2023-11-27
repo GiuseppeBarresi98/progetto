33f29d123fc2f99dc58acff7d7a5c4be08bb804b
GiuseppeBarresi98 <145044894+GiuseppeBarresi98@users.noreply.github.com>
2023-11-27 15:15:48 +0100

    first

---
+++ src/app/compenente1/compenente1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compenente1',
+  templateUrl: './compenente1.component.html',
+  styleUrls: ['./compenente1.component.scss']
+})
+export class Compenente1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
